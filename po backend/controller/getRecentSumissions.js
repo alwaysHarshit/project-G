d18e613f11b91d5ecc22dfd0679e7ddf20c7d646
badmoosBarua <studyharshit21@gmail.com>
2025-06-12 00:21:53 +0530

    fix v2.1

--- po backend/controller/getRecentSumissions.js
+++ po backend/controller/getRecentSumissions.js
@@ -3,6 +3,13 @@ import {fetchRecentSubmissions} from "../utils/leetCode.js";
 export const getRecentSubmissions = async (req, res) => {
     const {sessionId, username} = req.body;
     const recentSubmissions = await fetchRecentSubmissions(sessionId, username);
+    console.log(recentSubmissions.map(recentSubmission => ({
+        id: recentSubmission.id,
+        url: recentSubmission.url,
+        title: recentSubmission.title,
+        statusDisplay: recentSubmission.statusDisplay,
+        timestamp: recentSubmission.timestamp
+    })), "Recent Submissions Data Fetched Successfully!");
 
     const formattedSubmissions = recentSubmissions
         .map(submission => ({
@@ -10,11 +17,17 @@ export const getRecentSubmissions = async (req, res) => {
             url: submission.url,
             title: submission.title,
             status: submission.statusDisplay,
-            timestamp: submission.timestamp
+            timestamp:new Date(submission.timestamp).toLocaleString("en-IN", {
+                day: "2-digit",
+                month: "long",
+                year: "numeric",
+                hour: "numeric",
+                minute: "2-digit",
+                hour12: true,
+            })
         }))
-        .sort((a, b) => b.timestamp - a.timestamp);// most recent first
 
-    //console.log(formattedSubmissions)
+    //console.log(formattedSubmissions, "Formatted Submissions Data Fetched Successfully!")
     return res.status(201).json(formattedSubmissions);
 
 }
